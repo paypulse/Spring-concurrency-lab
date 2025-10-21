b91d072c9da68ef3eaf1249b8aa169fbaf40ba83
paypulse <sourcedev5@gmail.com>
2025-10-21 17:22:06 +0900

    [수정/추가] 낙관적 락 테스트 코드 수정 필요

--- src/test/java/com/example/springconcurrencylab/api/OptimisticLockConcurrencyTest.java
+++ src/test/java/com/example/springconcurrencylab/api/OptimisticLockConcurrencyTest.java
@@ -3,14 +3,20 @@ package com.example.springconcurrencylab.api;
 
 import com.example.springconcurrencylab.Api.LabLack.Dto.ClassScheduleRequestDto;
 import com.example.springconcurrencylab.Api.LabLack.Repository.ClassScheduleRepository;
+import com.example.springconcurrencylab.Api.LabLack.Service.LabLackService;
 import com.example.springconcurrencylab.Define.EntityEnum;
 import com.example.springconcurrencylab.Entity.ClassSchedule;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @SpringBootTest
@@ -23,6 +29,8 @@ class OptimisticLockConcurrencyTest {
     private ClassScheduleRepository classScheduleRepository;
 
     private Long testClassId;
+    @Autowired
+    private LabLackService labLackService;
 
     @BeforeEach
     void setUp() {
@@ -33,8 +41,38 @@ class OptimisticLockConcurrencyTest {
                 .classStatus(EntityEnum.ClassStatus.ONGOING)
                 .build();
 
-        classScheduleRepository.saveClassSchedule(schedule);
+        testClassId = classScheduleRepository.saveClassSchedule(schedule);
+        log.info("테스트 수업 생성 :  id = {}" , testClassId);
 
+    }
+
+    @Test
+    void testOptimisticLock_concurrentUpdate() throws Exception {
+        // 동시 접속 수
+        int threadCount  = 5;
+        // 쓰레드 공장 , 고정 쓰레드 풀 생성
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        //문지기, 동기화 도구
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        int count  = 0;
+
+        //TODO. 수정 요망
+        try {
+            do {
+                executorService.submit(() -> {
+                    labLackService.postIdEndClass(testClassId);
+                });
+
+                count ++;
+            }while(count < threadCount);
+        } catch(Exception e){
+            log.error("Excpetion occurred : {}", e.getMessage());
+        } finally{
+            countDownLatch.countDown();
+        }
+
+        countDownLatch.await();
 
 
 
