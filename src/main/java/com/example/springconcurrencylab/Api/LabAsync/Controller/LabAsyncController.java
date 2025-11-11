e999f13bd915df18967c590c6b2dea3c459aa37e
paypulse <sourcedev5@gmail.com>
2025-11-11 18:17:48 +0900

    [수정/추가] 비동기 예제 시작

--- src/main/java/com/example/springconcurrencylab/Api/LabAsync/Controller/LabAsyncController.java
+++ src/main/java/com/example/springconcurrencylab/Api/LabAsync/Controller/LabAsyncController.java
@@ -1,4 +1,15 @@
 package com.example.springconcurrencylab.Api.LabAsync.Controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "lab.async", description = "동시성 제어")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/labAsync")
+@Slf4j
 public class LabAsyncController {
 }
