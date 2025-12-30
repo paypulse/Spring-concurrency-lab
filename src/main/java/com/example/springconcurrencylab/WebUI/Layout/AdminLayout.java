2eeb6465f983424d1e913a8e77f64008fbdf97ab
paypulse <sourcedev5@gmail.com>
2025-12-30 17:05:24 +0900

    [수정/추가] Vaadin app button style 추가

--- src/main/java/com/example/springconcurrencylab/WebUI/Layout/AdminLayout.java
+++ src/main/java/com/example/springconcurrencylab/WebUI/Layout/AdminLayout.java
@@ -4,15 +4,35 @@ import com.example.springconcurrencylab.WebUI.LabAsync.LabAsyncView;
 import com.example.springconcurrencylab.WebUI.LabLack.LabLackView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouterLink;
+
+import static com.example.springconcurrencylab.WebUI.Layout.AppStyle.menuButton;
 
 public class AdminLayout extends AppLayout {
 
     public AdminLayout() {
-        addToNavbar(new H1("Concurrency Lab Admin"));
+        //<editor-fold desc="[vaddin] Navbar  영역">
+        H1 title = new H1("Concurrency Lab Admin");
+        title.getStyle()
+                .set("font-size", "var(--lumo-font-size-l)")
+                        .set("margin", "0");
+
+        HorizontalLayout navBar = new HorizontalLayout(title);
+        navBar.setWidthFull();
+        navBar.setAlignItems(FlexComponent.Alignment.CENTER);
+        navBar.setPadding(true);
+        addToNavbar(navBar);
+        //</editor-fold desc="[vaddin] Navbar  영역">
+
+        //<editor-fold desc="[vaddin] Drawer 영역">
         addToDrawer(
-                new RouterLink("Async 실험", LabAsyncView.class),
-                new RouterLink("Lack 실험", LabLackView.class)
+                menuButton("Async 실험", LabAsyncView.class)
+             //   new RouterLink("Lack 실험", LabLackView.class)
         );
+        //</editor-fold desc="[vaddin] Drawer 영역">
     }
+
+
 }
