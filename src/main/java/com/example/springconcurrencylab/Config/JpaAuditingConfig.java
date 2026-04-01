4e28fd721161891411ae907c5db1d0e98cd3d9e1
엄마는개발자 <sourcedev5@gmail.com>
2026-04-01 22:54:11 +0900

    [추가] jpa auditing config 추가

--- src/main/java/com/example/springconcurrencylab/Config/JpaAuditingConfig.java
+++ src/main/java/com/example/springconcurrencylab/Config/JpaAuditingConfig.java
@@ -1,11 +1,17 @@
 package com.example.springconcurrencylab.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
 
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("SYSTEM");
+    }
 
 }
